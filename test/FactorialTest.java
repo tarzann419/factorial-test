41b0ee701e2d78f1558052f1260e5233ac3b2af4
dantheman <danogbo0@gmail.com>
2023-10-13 08:40:09 +0100

    recent commit

--- test/FactorialTest.java
+++ test/FactorialTest.java
@@ -1,13 +1,25 @@
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class FactorialTest {
+    private Factorial factorial;
+    @Before
+    public void setUp(){
+        factorial = new Factorial();
+    }
     @Test
     public void calculate_0(){
-    //   first, create an instance of the class that you want to test
-        Factorial factorial = new Factorial();
         int testStartValue = 0;
         int expectedResult = 0;
+
+        assertEquals(expectedResult, factorial.calculateFactorial(testStartValue));
+    }
+
+    @Test
+    public void calculate_1(){
+        int testStartValue = 1;
+        int expectedResult = 1;
 
         assertEquals(expectedResult, factorial.calculateFactorial(testStartValue));
     }
